6c884f911901a66dabf60c8e78bf7140f0d8d7b7
Igor Nichiforov <70261890+Igor-Nix@users.noreply.github.com>
2022-05-06 20:38:39 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+import {
+  PerspectiveCamera,
+  OrbitControls,
+  CubeCamera,
+  Environment,
+} from "@react-three/drei";
+import { Ground } from "./Ground";
+import { Car } from "./Car";
+import { Rings } from "./Rings";
 
-function App() {
+function CarShow() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <color args={[0, 0, 0]} attach="background" />
+
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => {
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>;
+        }}
+      </CubeCamera>
+      <Rings />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
+  );
+}
